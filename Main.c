656cce871e15e598b183c2f455057677179a31a3
Zia Beg <ziabeg@hotmail.com>
2024-04-04 10:14:51 +0200

    sdl2 supposedly works

--- Main.c
+++ Main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL.h>
 
-int main() {
+typedef struct CardStruct {
+    int number;
+    char suit;
+    char displayedChars[2];
+    struct CardStruct* next;
+    bool facingDir;
+} Card;
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Quit();
+
     printf("Hello world");
     return 0;
 }
